282bafbd5ccd58a25aad960b25c690cd81b99efb
artsemKorotkevich <artsem.karatkevich@innowise-group.com>
2020-08-20 17:18:10 +0300

    fix

--- src/main/java/by/innowise/by/innowise/taskService/entities/User.java
+++ src/main/java/by/innowise/by/innowise/taskService/entities/User.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 @NoArgsConstructor
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
